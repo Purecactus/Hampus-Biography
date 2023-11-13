3878a70c8c19f73f92be9806bca2f77520d5724c
Purecactus <zaha23ct@student.ju.se>
2023-11-13 17:49:38 +0100

    Added img and button with js

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+<script>
+let popup = document.getElementById(popup1);
+
+function openPopup1(){
+    popup.classList.add("open-popup1")}
+
+function closePopup1(){
+    popup.classList.remove("open-popup1")}
+</script>
